68601fffc618d039a2887d2943a554549282829a
pokeshot <hossa123>
2014-11-16 20:19:34 +0100

    jive instance and user update

---
+++ Net.Pokeshot.JiveSdk/Models/Dto/JiveInstanceDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.Pokeshot.JiveSdk.Models.Dto
+{
+ 
+        [DataContract]
+        public class JiveInstanceDto
+        {
+            [Required]
+            [DataMember(IsRequired = true)]
+            public string name { get; set; }
+            [Required]
+            [DataMember(IsRequired = true)]
+            public string jiveUrl { get; set; }
+
+            [DataMember]
+            public string version { get; set; }
+            [Required]
+            [DataMember(IsRequired = true)]
+            public string communityLanguage { get; set; }
+        }
+    
+}
