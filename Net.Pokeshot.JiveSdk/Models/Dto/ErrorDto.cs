59de48ceeaf3a8e5bde9cecb1cab9fba19efcf18
pokeshot <hossa123>
2014-11-18 00:09:55 +0100

    error dto addd

---
+++ Net.Pokeshot.JiveSdk/Models/Dto/ErrorDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.Pokeshot.JiveSdk.Models.Dto
+{
+    public class ErrorDto
+    {
+        public Error error { get; set; }
+    }
+
+    public class Error
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+    }
+}
