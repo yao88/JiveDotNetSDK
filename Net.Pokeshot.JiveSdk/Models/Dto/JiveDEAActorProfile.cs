db90168625e336925b29d0561a44cfe1a218fa41
pokeshot <hossa123>
2014-11-27 01:13:21 +0100

    Jive Data Export API Object Types added

---
+++ Net.Pokeshot.JiveSdk/Models/Dto/JiveDEAActorProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Net.Pokeshot.JiveSdk.Models.Dto
+{
+    public class JiveDEAActorProfile
+    {
+        public string hasAvatar { get; set; }
+
+    }
+}
---
+++ Net.Pokeshot.JiveSdk/Models/Dto/JiveDEADestination.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Net.Pokeshot.JiveSdk.Models.Dto
+{
+    public class JiveDEADestination
+    {
+        public string @class { get; set; }
+        public string objectType { get; set; }
+        public int objectId { get; set; }
+        public  string objectHash {get;set;}
+        public bool isDataAvailable {get;set;}
+        public List<string> tags {get;set;}
+        public string name {get;set;}
+        public string displayName {get;set;}
+        public string description {get;set;}
+        public int creationDate {get;set;}
+        public int modificationDate {get;set;}
+        public string url {get;set;}
+        public string status {get;set;}
+        public bool isVisibleToPartner {get;set;}
+        public string parentId {get;set;}
+        public string parentType {get;set;}
+        public string fullPath {get;set;}
+        public string normalizedPath{get;set;}
+        public string combinedObjectTypeId {get;set;}
+    }
+}
