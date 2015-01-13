b3564aa8ec5810dbae85879dee6c6cf0d7327a08
Nils Heuer <jivesales@pokeshot.net>
2015-01-13 13:30:48 +0100

    chnaged peoplelist

--- Net.Pokeshot.JiveSdk/Models/PeopleList.cs
+++ Net.Pokeshot.JiveSdk/Models/PeopleList.cs
@@ -9,7 +9,7 @@ namespace Net.Pokeshot.JiveSdk.Models
     public class PeopleList
     {
         public int itemsPerPage { get; set; }
-        public ListLinks links { get; set; }
+        //public ListLinks links { get; set; }
         public List<Person> list { get; set; }
     }
 }
