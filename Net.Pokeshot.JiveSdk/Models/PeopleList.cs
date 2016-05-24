7b7b86c78121a846cd36f98fd167a24bbe55ecc0
yao88 <peteryao@hotmail.com>
2016-05-24 15:29:04 -0400

    Update PeopleList.cs
    This allows deserialization of links.   Removed unused references.

--- Net.Pokeshot.JiveSdk/Models/PeopleList.cs
+++ Net.Pokeshot.JiveSdk/Models/PeopleList.cs
@@ -1,15 +1,16 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Net.Pokeshot.JiveSdk.Models
 {
     public class PeopleList
     {
         public int itemsPerPage { get; set; }
-        //public ListLinks links { get; set; }
+        public JiveLinkResource links { get; set; }
         public List<Person> list { get; set; }
     }
+    
+    public class JiveLinkResource
+    {
+        public string next { get; set; }
+    }    
 }
